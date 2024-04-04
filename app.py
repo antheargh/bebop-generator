36184c76002c2904faedc18854d1774db3fc97a2
anthea <32894346+antheargh@users.noreply.github.com>
2024-04-04 14:58:32 +1100

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def hello():
     return render_template("home.html")
 
 
-@app.route("/card", methods=['GET', s"POST"])
+@app.route("/card", methods=["GET", "POST"])
 def render_card():
     text = request.form["cardText"]
     w = int(request.form["cardWidth"])
